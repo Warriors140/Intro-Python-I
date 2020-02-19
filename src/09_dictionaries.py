9c925f25534f7230177c24072e2929298f369e12
Randy Warrington <warrington.randy@outlook.com>
2020-02-18 19:33:59 -0500

    sorry for being late, finished project

--- src/09_dictionaries.py
+++ src/09_dictionaries.py
@@ -35,6 +35,12 @@ waypoints = [
 
 # Add a new waypoint to the list
 # YOUR CODE HERE
+waypoint.append({
+    "lat": 50,
+    "lon": -132,
+    "name": "fourth place"
+})
+print(waypoints)
 
 # Modify the dictionary with name "a place" such that its longitude
 # value is -130 and change its name to "not a real place"
@@ -42,6 +48,12 @@ waypoints = [
 # waypoints list.
 
 # YOUR CODE HERE
+waypoints[0].update({"lon": -130})
+waypoints[0].update({"name": "not a real place"})
+
+print(waypoints)
 
 # Write a loop that prints out all the field values for all the waypoints
 # YOUR CODE HERE
+for v in waypoints:
+    print(v)
--- src/11_args.py
+++ src/11_args.py
@@ -5,6 +5,8 @@
 # the sum. This is what you'd consider to be a regular, normal function.
 
 # YOUR CODE HERE
+def f1(a, b):
+    return a + b
 
 print(f1(1, 2))
 
@@ -13,6 +15,8 @@ print(f1(1, 2))
 # Note: Google for "python arbitrary arguments" and look for "*args"
 
 # YOUR CODE HERE
+def f2(*args):
+    return sum(args)
 
 print(f2(1))                    # Should print 1
 print(f2(1, 3))                 # Should print 4
@@ -30,6 +34,10 @@ print(f2(a))    # Should print 22
 # Note: Google "python default arguments" for a hint.
 
 # YOUR CODE HERE
+def f3(arg1, arg2=None):
+    if arg2 is None:
+        return arg1 + 1
+    return arg1 + arg2
 
 print(f3(1, 2))  # Should print 3
 print(f3(8))     # Should print 9
@@ -44,6 +52,9 @@ print(f3(8))     # Should print 9
 # Note: Google "python keyword arguments".
 
 # YOUR CODE HERE
+def f4(**kwargs):
+    for k, v in kwargs.items():
+        print(f'key: {k}, value: {v}')
 
 # Should print
 # key: a, value: 12
